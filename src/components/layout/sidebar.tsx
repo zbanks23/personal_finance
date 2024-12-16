13531071674ee401bfafcb2aeb29f15ff7955002
Zhicheng <zhicheng19edu@gmail.com>
2024-12-16 11:57:40 -0500

    fixed deployment error for vercel

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,7 +1,6 @@
 // src/components/layout/Sidebar.tsx
 "use client";
 
-import React, { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
