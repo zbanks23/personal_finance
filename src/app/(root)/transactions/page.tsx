441440fda5f1f4a8fabb192e24c53357b4a519f6
Zhicheng <zhicheng19edu@gmail.com>
2024-12-16 21:40:43 -0500

    Set up postgres and connected to DB

--- src/app/(root)/transactions/page.tsx
+++ src/app/(root)/transactions/page.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { connectToDB } from "@/lib/data";
 
-const transactions = () => {
+export default async function transactions() {
+  const client = await connectToDB();
+
   return (
     <div className="w-3/4">
       <h1 className="text-indigo-600 text-2xl font-bold mb-4">
         Transactions History
       </h1>
-      {/* Additional content for your home page */}
+      {client && <p className="text-green-500">Connected to database</p>}
     </div>
   );
-};
-
-export default transactions;
+}
